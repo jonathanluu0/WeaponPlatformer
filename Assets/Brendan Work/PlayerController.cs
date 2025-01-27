aa9a16a2a656d6fe1690138708cbe8aa2266a270
ktleong2000 <brendanleong105@gmail.com>
2025-01-26 20:57:35 -0800

    Revert "Changed some files"
    This reverts commit 46cc679cb6f822726b5f3b353da00a80037efedc.

--- Assets/Brendan Work/PlayerController.cs
+++ Assets/Brendan Work/PlayerController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TestPlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 5f; // Adjust for jumping power
     private Rigidbody2D rb;
